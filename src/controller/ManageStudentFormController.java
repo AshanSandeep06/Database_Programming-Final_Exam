1bb76b9608a7217f8c43500e7f1cfb2cfb673ab3
AshanSandeep06 <ashansandeep06@gmail.com>
2022-07-12 11:03:16 +0530

    Refactored ManageStudentForm.fxml and it's controller class

--- src/controller/ManageStudentFormController.java
+++ src/controller/ManageStudentFormController.java
@@ -1,5 +1,13 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
 /**
  * @author : Ashan Sandeep
  * @since : 0.1.0
@@ -7,4 +15,49 @@ package controller;
 
 public class ManageStudentFormController {
 
+    public JFXTextField txtStudentID;
+    public JFXButton btnSearch;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtNic;
+    public JFXButton btnUpdate;
+    public JFXButton btnRemove;
+    public TableView tblStudent;
+    public TableColumn colStudentID;
+    public TableColumn colName;
+    public TableColumn colNIC;
+    public TableColumn colAddress;
+    public TableColumn colEmail;
+    public TableColumn colMobileNumber;
+    public JFXTextField txtAddress;
+    public JFXButton btnAdd;
+    public TextField txtSearchStudent;
+    public JFXTextField txtEmail;
+    public JFXTextField txtMobileNumber;
+
+    public void searchStudentOnAction(ActionEvent event) {
+    }
+
+    public void validateKeyReleasedOnAction(KeyEvent keyEvent) {
+    }
+
+    public void updateStudentOnAction(ActionEvent event) {
+    }
+
+    public void removeStudentOnAction(ActionEvent event) {
+    }
+
+    public void resetOnAction(ActionEvent event) {
+    }
+
+    public void addStudentOnAction(ActionEvent event) {
+    }
+
+    public void txtSearchStudentOnAction(ActionEvent event) {
+    }
+
+    public void btnAddNewStudentOnAction(ActionEvent event) {
+    }
+
+    public void searchStudentKeyReleasedOnAction(KeyEvent keyEvent) {
+    }
 }
