03b9560ce6cab7a58139400333d549c44fa77118
AshanSandeep06 <ashansandeep06@gmail.com>
2022-07-12 09:00:12 +0530

    Create DBConnection class to estabilished a single database connection

---
+++ src/db/DBConnection.java
@@ -0,0 +1,28 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author : Ashan Sandeep
+ * @since : 0.1.0
+ **/
+
+public class DBConnection {
+    private static DBConnection dbConnection;
+    private final Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/ijse", "root", "1234");
+    }
+
+    public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+        return (dbConnection == null) ? dbConnection = new DBConnection() : dbConnection;
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+}
